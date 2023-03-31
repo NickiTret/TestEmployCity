757a8cc1a6c734db4b966a7eb3e5f87fc1c0630b
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-03-31 17:33:47 +0300

    delete files all

--- src/js/_components.js
+++
@@ -1,8 +0,0 @@
-// import './components/slider.js';
-// import './components/fancybox.js';
-// import './components/modal.js';
-// import './components/animate.js';
-// import './components/validate.js';
-// import './components/ymap.js';
-import './components/select.js';
-import './components/burger.js';
